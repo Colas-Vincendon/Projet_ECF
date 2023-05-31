bc544d5747e49c27644b8a9d21002769820c4232
Colas <colasvincendon@gmail.com>
2023-05-31 22:05:42 +0200

    finished carRegistration.html

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,8 @@
 $("li.dropdown").hover(
   function () {
-    $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeIn(500);
+    $(this).find(".dropdown-menu").stop(true, true).delay(100).fadeIn(400);
   },
   function () {
-    $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeOut(500);
+    $(this).find(".dropdown-menu").stop(true, true).delay(100).fadeOut(400);
   }
 );
