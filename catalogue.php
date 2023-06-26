a307376782a2ff6b04cd21464e95407bcf4f0881
Colas <colasvincendon@gmail.com>
2023-06-26 17:21:43 +0200

    create filtersBrand.php

--- catalogue.php
+++ catalogue.php
@@ -340,10 +340,10 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
     integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
     crossorigin="anonymous"></script>
+    <script src="src/scripts/filtersBrand.js"></script>
   <script src="./src/scripts/script.js"></script>
   <script src="src/scripts/scroll.js"></script>
   <script src="src/scripts/schedules.js"></script>
-  <script src="src/scripts/filtersBrand.js"></script>
 </body>
 
 </html>
