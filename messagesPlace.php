ce27bb609d3ab7f875ddfc5362363e20d7068149
Colas <colasvincendon@gmail.com>
2023-06-26 12:54:51 +0200

    modify php/html mix code in messagesPlace.php

--- messagesPlace.php
+++ messagesPlace.php
@@ -146,15 +146,8 @@
                 <div class="container-fluid text-center">
                     <div class="row">
                         <?php
-                        // Connexion à la base de données
-                        $servername = "eu-cdbr-west-03.cleardb.net";
-                        $username = "b3b93f93ef4872";
-                        $password = "21163a70";
-                        $dbname = "heroku_a9b8c2ad4d5e1ab";
-
-                        try {
-                            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        
+                        require_once 'databaseConnexion.php';
 
                             // Récupérer tous les messages de la table "messages"
                             $sql = "SELECT * FROM messages";
@@ -206,9 +199,6 @@
                             }
 
                             $conn = null;
-                        } catch (PDOException $e) {
-                            echo "Échec de la connexion à la base de données : " . $e->getMessage();
-                        }
                         ?>
 
                             </div>
