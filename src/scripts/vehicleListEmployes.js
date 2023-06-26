9d77d473dd8c488ef283d9ff0b6d4ca497fc1725
Colas <colasvincendon@gmail.com>
2023-06-26 13:37:29 +0200

    modify vehicleList.php to .html

--- src/scripts/vehicleListEmployes.js
+++
@@ -1,6 +0,0 @@
-/*------------- affichage des vehicules sur page vehicleList.html ---------------------*/
-
-$(document).ready(function () {
-  // Charger le contenu
-  $("#vehicleList").load("vehicleListEmployes.php #displayVehicleListEmployes");
-});
