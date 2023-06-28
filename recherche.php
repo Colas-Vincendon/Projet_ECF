2d6bfa7221cafa1424559929248e77d1dc56526f
Colas <colasvincendon@gmail.com>
2023-06-28 15:50:38 +0200

    create pagination catalogue.php

--- recherche.php
+++ recherche.php
@@ -163,7 +163,7 @@ if ($tri === "kilometres_asc") {
     }
 
     // Affichage de la pagination
-    echo "<div class='container-fluid mx-0'>";
+    echo "<div class='container-fluid mx-0 px-0'>";
     echo "<ul class='paginationCustom px-0 mt-3'>";
 
     if ($totalPages > 1) {
