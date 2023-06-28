611fe78ed8fdee49a548557f87427e99895d7ea0
Colas <colasvincendon@gmail.com>
2023-06-28 11:46:15 +0200

    create pagination catalogue.php

--- recherche.php
+++ recherche.php
@@ -192,7 +192,7 @@ if ($tri === "kilometres_asc") {
         for ($i = 1; $i <= $totalPages; $i++) {
             $active_class = "";
             if ($i == $currentPage) {
-                $active_class = "activeCustom";
+                $active_class = "active";
             }
             echo "<li class='page-item " . $active_class . "' id='" . $i . "'><span class='page-link'>" . $i . "</span></li>";
         }
