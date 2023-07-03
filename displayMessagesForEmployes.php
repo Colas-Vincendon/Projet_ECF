2f067ecbaac467ce470ec1930d22de3878c658d8
Colas <colasvincendon@gmail.com>
2023-07-03 12:17:42 +0200

    added singleton (databaseConnexion) all PHP pages

--- displayMessagesForEmployes.php
+++ displayMessagesForEmployes.php
@@ -1,32 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Projet ECF: Garage Parrot</title>
-    <meta
-      type="description"
-      content="Entretien de votre véhicule et vente de véhicules d'occasion"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-      crossorigin="anonymous"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css"
-    />
-    <link rel="stylesheet" href="./src/styles/style.css" />
-  </head>
-  <body>
-  <div id="messagesEmployes">
+<div id="messagesEmployes">
     <?php
 
     require_once 'databaseConnexion.php';
@@ -86,17 +58,8 @@
     $conn = null;
     ?>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-      integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-      integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-      crossorigin="anonymous"
-    ></script>
-    <script src="src/scripts/script.js"></script>
-  </body>
-</html>
+    <script>
+        function confirmDeleteMessage() {
+  return confirm("Voulez-vous vraiment supprimer ce message ?");
+}
+    </script>
