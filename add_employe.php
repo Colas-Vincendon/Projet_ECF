a21accc31bb03616ad87ccb08d6da5821f61d96c
Colas <colasvincendon@gmail.com>
2023-07-03 09:42:30 +0200

    testing singleton

--- add_employe.php
+++ add_employe.php
@@ -6,35 +6,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     // Connexion à la base de données
-    $servername = 'eu-cdbr-west-03.cleardb.net';
-    $dbname = 'heroku_a9b8c2ad4d5e1ab';
-    $username = 'b3b93f93ef4872';
-    $password2 = '21163a70';
+    require_once 'databaseConnexion.php';
+    //SINGLETON
+    $database = Database::getInstance();
+    $conn = $database->getConnection();
 
-    try {
-        // Connexion à la base de données en utilisant PDO
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password2);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Hachage du mot de passe
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        // Hachage du mot de passe
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    // Requête d'insertion de l'employé avec des paramètres préparés
+    $query = "INSERT INTO employes (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':password', $hashedPassword);
 
-        // Requête d'insertion de l'employé avec des paramètres préparés
-        $query = "INSERT INTO employes (email, password) VALUES (:email, :password)";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':password', $hashedPassword);
-
-        // Exécuter la requête
-        if ($stmt->execute()) {
-            // Redirection vers la page d'accueil de l'administrateur
-            header('Location: newEmployeAdded.html');
-            exit();
-        } else {
-            echo "Erreur lors de l'ajout à la base de données";
-        }
-    } catch (PDOException $e) {
-        die("Erreur de connexion à la base de données : " . $e->getMessage());
+    // Exécuter la requête
+    if ($stmt->execute()) {
+        // Redirection vers la page d'accueil de l'administrateur
+        header('Location: newEmployeAdded.html');
+        exit();
+    } else {
+        echo "Erreur lors de l'ajout à la base de données";
     }
 
     // Fermer la connexion à la base de données
