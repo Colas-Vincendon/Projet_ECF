8988cf1b699f1d9b806d4032e2fc2a883c94048e
Colas <colasvincendon@gmail.com>
2023-07-03 09:45:29 +0200

    testing singleton

--- add_review.php
+++ add_review.php
@@ -20,15 +20,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['envoiReview'])) {
 }
 
 // Connexion à la base de données
-$servername = "eu-cdbr-west-03.cleardb.net";
-$username = "b3b93f93ef4872";
-$password = "21163a70";
-$dbname = "heroku_a9b8c2ad4d5e1ab";
-
-try {
-    // Connexion à la base de données en utilisant PDO
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once 'databaseConnexion.php';
+          //SINGLETON
+          $database = Database::getInstance();
+          $conn = $database->getConnection();
 
     // Récupérer les valeurs du formulaire
     $nom = utf8_encode($_POST['nom']);
@@ -49,11 +44,7 @@ try {
     // Rediriger vers la page index.php ou une autre page de confirmation
     header("Location: commentSent.html");
     exit();
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-}
 
 // Fermer la connexion à la base de données
-
 
 ?>
